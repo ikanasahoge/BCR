f3e600f71428df86a31893e49087a0b19af89a2f
ikanasahoge <ikanasahoge@gmail.com>
2026-05-06 14:33:23 +0200

    Add files via upload

---
+++ api/login.js
@@ -0,0 +1,17 @@
+export default function handler(req, res) {
+  const clientId = process.env.SPOTIFY_CLIENT_ID;
+  const redirectUri = process.env.SPOTIFY_REDIRECT_URI;
+
+  const state = Math.random().toString(36).slice(2);
+  const scopes = 'user-read-currently-playing user-read-playback-state';
+
+  const params = new URLSearchParams({
+    response_type: 'code',
+    client_id: clientId,
+    scope: scopes,
+    redirect_uri: redirectUri,
+    state,
+  });
+
+  res.redirect('https://accounts.spotify.com/authorize?' + params.toString());
+}
